9532f8fbcdb763b3bb1517f9aae3c954c2c5339b
David Ke <93749669+DavidKe410@users.noreply.github.com>
2024-07-09 17:54:44 -0500

    Add files via upload

---
+++ run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+gnome-terminal -- bash -c "source ~/air_dmg_assesment_ws/devel/setup.bash && roslaunch flir_boson_usb flir_boson.launch dev:=/dev/video1 && rosparam load ~/ardu_tof_bridge_ws/src/ros1_bridge/bridge.yaml; exec bash"
+
+sleep 3
+
+gnome-terminal -- bash -c "source ~/air_dmg_assesment_ws/devel/setup.bash && roslaunch realsense2_camera rs_camera.launch enable_gyro:=true enable_accel:=true unite_imu_method:=linear_interpolation align_depth:=true; exec bash"
+
+sleep 3
+
+gnome-terminal -- bash -c "source ardu_tof_bridge_ws/install/setup.bash && ros2 run arducam tof_pointcloud; exec bash"
+
+sleep 3
+
+gnome-terminal -- bash -c "source ardu_tof_bridge_ws/install/setup.bash && ros2 run ros1_bridge parameter_bridge; exec bash"
+
+sleep 3
+
+gnome-terminal -- bash -c "source /opt/ros/noetic/setup.bash && rosrun tf static_transform_publisher 0 0 0.075 0.5 0.5 0.5 -0.5 sensor_frame camera_link 100; exec bash"
